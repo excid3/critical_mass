1d25c11be7bac2fede5fe96a2467d8b0c07e986d
Chris Oliver <chris@Chriss-MacBook-Air.local>
2011-10-18 21:06:33 -0500

    Commit all the things

--- Critical Mass/AppDelegate.rb
+++ Critical Mass/AppDelegate.rb
@@ -3,13 +3,67 @@
 #  Critical Mass
 #
 #  Created by Chris Oliver on 10/13/11.
-#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#  Copyright 2011 Chris Oliver. All rights reserved.
 #
 
 class AppDelegate
-    attr_accessor :window
+    attr_accessor :window, :power_source_label, :status_label, :status_detail_label, :battery_percent_label, :battery_level
+    
     def applicationDidFinishLaunching(a_notification)
-        # Insert code here to initialize your application
+        window.level = NSFloatingWindowLevel
+        @hide = 4
+        
+        tick
+        @timer = NSTimer.scheduledTimerWithTimeInterval 1, target:self, selector: :tick, userInfo:nil, repeats:true
+
+    end
+    
+    def tick
+        # Hide the window after 
+        @hide -= 1 if @hide >= 0
+        window.orderOut self if @hide == 0
+        
+        status = battery_status
+        percent = status[:percent]
+        
+        battery_level.setFloatValue percent.to_f
+        
+        power_source_label.stringValue = status[:source]
+        battery_percent_label.stringValue = "#{percent}% " + full_or_remaining(status[:status])
+        
+        
+        if status[:status] == "discharging" and percent.to_i <= 5
+            status_label.stringValue = "Low battery"
+            status_detail_label.stringValue = "Quick! Hurry up and plug in!"
+            window.orderFront self
+        elsif status[:source] == "AC Power" and @hide < 0 # no longer starting up
+            status_label.stringValue = "Charging"
+            status_detail_label.stringValue = "Phew, you're safe for now."
+            @hide = 3
+        end
+    end
+    
+    def full_or_remaining(status)
+        (status == "charging") ? "Full" : "Remaining"
+    end
+    
+    def battery_output
+        `pmset -g batt`.split "\n"
+    end
+        
+    def battery_status
+        source, battery, *batteries = battery_output
+        power_source = source.match(/.+'(.+)'$/)[1]
+        
+        name, status_line = battery.split "\t"
+        percent, status, time = status_line.split "; "
+        
+        {
+            :source => power_source, 
+            :percent => percent.match(/\d+/)[0],
+            :status => status, 
+            :time => time
+        }
     end
 end
 
